57444b0031f096d35d6eb53f4788fdaeda6d8ef2
stslex <ilya977.077@gmail.com>
2023-08-25 21:23:34 +0300

    add test routes

--- src/routes/routes.rs
+++ src/routes/routes.rs
@@ -2,7 +2,7 @@ use rocket_contrib::json::Json;
 
 use crate::database;
 use crate::routes::route_objects::ApiResponse;
-use crate::routes::route_objects::error_response::{ERROR_ALREADY_REGISTERED, ERROR_UNKNOWN, ERROR_USER_NOT_FOUND, ERROR_WEAK_PASSWORD, ERROR_WRONG_REQUEST};
+use crate::routes::route_objects::error_response::ERROR_WRONG_REQUEST;
 use crate::routes::route_objects::login_request::LoginRequest;
 use crate::routes::route_objects::registration_request::RegistrationRequest;
 
@@ -21,5 +21,21 @@ pub fn registration(
     db: database::Conn,
 ) -> ApiResponse<'static, ()> {
     // TODO add registration
+    ApiResponse::Err(ERROR_WRONG_REQUEST)
+}
+
+#[get("/hello/<name>")]
+pub fn helloUsername(name: String) -> &'static str {
+    let formated_hello = format!("{}{}", "hello ".to_owned(), name);
+    Box::leak(formated_hello.to_string().into_boxed_str())
+}
+
+#[get("/hello")]
+pub fn hello() -> &'static str {
+    "Hello"
+}
+
+#[get("/error")]
+pub fn error() -> ApiResponse<'static, ()> {
     ApiResponse::Err(ERROR_WRONG_REQUEST)
 }
