17b836bf29b13104f9037ab249a16da26475d47b
stslex <ilya977.077@gmail.com>
2023-11-04 22:07:15 +0300

    refactor Ok user model

--- src/handlers/auth/objects.rs
+++ src/handlers/auth/objects.rs
@@ -1,20 +1,13 @@
-use rocket::request::Request;
-use rocket::response::Responder;
-use rocket::response::Result;
+use serde::Serialize;
 
 pub enum LoginError {
     NotFound,
     Other,
 }
 
+#[derive(Serialize)]
 pub struct LoginOk {
     pub uuid: String,
     pub username: String,
     pub token: String,
-}
-
-impl<'r> Responder<'r> for LoginOk {
-    fn respond_to(self, request: &Request) -> Result<'r> {
-        self.respond_to(request)
-    }
 }
