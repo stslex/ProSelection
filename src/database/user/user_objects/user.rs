2fc2e58b34b17af37eb4266e7ad2ea089c3e9f11
stslex <ilya977.077@gmail.com>
2023-11-04 14:47:34 +0300

    add jwt_objects

--- src/database/user/user_objects/user.rs
+++ src/database/user/user_objects/user.rs
@@ -1,6 +1,6 @@
 use uuid::Uuid;
 
-#[derive(Queryable, PartialEq, Debug)]
+#[derive(Queryable, PartialEq, Debug, Clone)]
 pub struct User {
     pub id: Uuid,
     pub username: String,
--- src/utils/jwt_utils.rs
+++ src/utils/jwt_utils.rs
@@ -1,4 +1,32 @@
-pub fn generate_jwt() -> &'static str {
-    // todo!("generate jwt");
-    "token"
+use uuid::Uuid;
+
+use crate::database::user::user_objects::user::User;
+
+pub struct JwtObject {
+    pub uuid: Uuid,
+    pub username: String,
+}
+
+pub trait JwtMapper {
+    fn map(&self) -> JwtObject;
+}
+
+impl JwtMapper for User {
+    fn map(&self) -> JwtObject {
+        JwtObject {
+            uuid: self.id,
+            username: self.username.clone(),
+        }
+    }
+}
+
+pub trait JwtUtil {
+    fn generate(&self) -> &'static str;
+}
+
+impl JwtUtil for JwtObject {
+    fn generate(&self) -> &'static str {
+        // todo!("generate jwt");
+        "token"
+    }
 }
