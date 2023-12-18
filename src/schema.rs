176a3be236580d6175e8de1e08336b96ed7b6ce8
stslex <ilya977.077@gmail.com>
2023-12-18 21:24:25 +0300

    add tables

--- src/schema.rs
+++ src/schema.rs
@@ -8,5 +8,32 @@ diesel::table! {
         login -> Varchar,
         username -> Varchar,
         secret -> Text,
+        avatar_url -> Varchar,
+        bio -> Varchar,
+    }
+}
+
+diesel::table! {
+    following (uuid, following_uuid){
+        uuid -> Uuid,
+        following_uuid -> Uuid,
+        username -> Varchar,
+        avatar_url -> Varchar,
+    }
+}
+
+diesel::table! {
+    followers (uuid, follower_uuid){
+        uuid -> Uuid,
+        follower_uuid -> Uuid,
+        username -> Varchar,
+        avatar_url -> Varchar,
+    }
+}
+
+diesel::table! {
+    favourite (uuid, favourite_uuid){
+        uuid -> Uuid,
+        favourite_uuid -> Uuid,
     }
 }
