daf83705e42edfa5450e5500b53f3c4c1e261394
stslex <ilya977.077@gmail.com>
2023-12-30 14:01:54 +0300

    fix run db

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,30 @@
-use rocket::Config;
+use std::collections::HashMap;
 use std::env;
 
+use dotenv::dotenv;
+use rocket::figment::value::Value;
+use rocket::Config;
+
 pub fn from_env() -> Config {
+    dotenv().ok();
+
     let port = env::var("PORT")
         .unwrap_or_else(|_| "8000".to_string())
         .parse::<u16>()
         .expect("PORT environment variable should parse to an integer");
 
-    let provider = rocket::Config::figment().merge(("port", port));
+    let mut database_config = HashMap::new();
+    let mut databases = HashMap::new();
+
+    let database_url =
+        env::var("DATABASE_URL").expect("No DATABASE_URL environment variable found");
+
+    database_config.insert("url", Value::from(database_url));
+
+    databases.insert("diesel_postgres_pool", database_config);
+
+    let provider = rocket::Config::figment()
+        .merge(("port", port))
+        .merge(("databases", databases));
     Config::from(provider)
 }
